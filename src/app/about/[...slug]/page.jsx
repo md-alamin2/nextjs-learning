c60bb30d88d2a30a84432b16bf15d00f91e79dbc
md-alamin2 <mdalamin22671@gmail.com>
2025-08-19 23:10:14 +0600

    format the code

--- src/app/about/[...slug]/page.jsx
+++ src/app/about/[...slug]/page.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
-export default async function AboutSlugPages({params}) {
-    const p = await params
-    console.log(p)
-  return (
-    <div>
-      AboutSlugPages
-    </div>
-  )
+export default async function AboutSlugPages({ params }) {
+  const p = await params;
+  console.log(p);
+  return <div>AboutSlugPages</div>;
 }
